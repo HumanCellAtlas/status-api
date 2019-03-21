b851de5812ded6ec3351f30f16607687c04f357f
Matt Weiden <538456+mweiden@users.noreply.github.com>
2019-03-20 17:39:34 -0700

    Enable collecting status of calculated health checks

--- cron.py
+++ cron.py
@@ -2,6 +2,7 @@ import calendar
 import datetime
 import logging
 import time
+from collections import namedtuple
 
 import boto3
 from botocore.exceptions import ClientError
@@ -13,41 +14,21 @@ cloudwatch = boto3.client('cloudwatch')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+HealthCheck = namedtuple('HealthCheck', 'name id healthy availability')
+
 
 def handler(event, context):
-    mappings = _get_tag_mappings()
-    for service_name, resource_id in mappings.items():
-        try:
-            response = route53.get_health_check_status(HealthCheckId=resource_id)
-        except ClientError as e:
-            if e.response.get("Error", {}).get("Code") == 'Throttling':
-                seconds = 2.0
-                logger.info(f"Hit rate limit. Sleeping for {seconds} seconds.")
-                time.sleep(seconds)
-                response = route53.get_health_check_status(HealthCheckId=resource_id)
-            elif e.response.get("Error", {}).get("Code") != 'InvalidInput':
-                raise e
-            else:
-                # Do not fail if this is an aggregated health check
-                logger.info(f"Skipping {service_name}")
-                continue
-
-        healthy = all([
-            ele['StatusReport']['Status'].startswith('Success')
-            for ele in response['HealthCheckObservations']
-        ])
-        status = "ok" if healthy else "error"
-        availability = _availability(resource_id)
-        dynamodb.put_item(
-            TableName='application_statuses',
-            Item={
-                'service_name': {'S': service_name},
-                'status': {'S': status},
-                'availability': {'N': str(availability)},
-                'time_to_exist': {'N': str(calendar.timegm(time.gmtime()) + 180)}
-            }
+    for health_check in _get_health_checks():
+        status = "ok" if health_check.healthy else "error"
+        item = dict(
+            service_name={'S': health_check.name},
+            status={'S': status},
+            time_to_exist={'N': str(calendar.timegm(time.gmtime()) + 180)}
         )
-        logger.info(f"Updated {service_name} ({resource_id}) to status \"{status}\", with availability {availability}")
+        if health_check.availability:
+            item['availability'] = dict(N=str(health_check.availability))
+        dynamodb.put_item(TableName='application_statuses', Item=item)
+        logger.info(f"Updated {health_check.name} ({health_check.id}) to status \"{status}\", with availability {health_check.availability}")
     logger.info("done")
 
 
@@ -80,6 +61,9 @@ def _availability(health_check_id):
         EndTime=end_date
     )['MetricDataResults']
     avg = next(ele for ele in data if ele['Id'] == 'avg')
+    if len(avg['Timestamps']) < 1:
+        logger.info("Skip availability calculation for {health_check_id}. This is likely a calculated health check.")
+        return None
     min_ind = avg['Timestamps'].index(min(avg['Timestamps']))
     availability = avg['Values'][min_ind]
     return availability
@@ -92,33 +76,60 @@ def _find_first(f, collection):
     return None
 
 
-def _get_tag_mappings():
-    health_check_ids = _get_health_check_ids()
-    return _ids_to_tag_mappings(health_check_ids)
+def _get_health_check_status(resource_id):
+    try:
+        response = route53.get_health_check_status(HealthCheckId=resource_id)
+    except ClientError as e:
+        if e.response.get("Error", {}).get("Code") != 'Throttling':
+            raise e
+        seconds = 5.0
+        logger.info(f"Hit rate limit. Sleeping for {seconds} seconds.")
+        time.sleep(seconds)
+        response = route53.get_health_check_status(HealthCheckId=resource_id)
+    return all([
+        ele['StatusReport']['Status'].startswith('Success')
+        for ele in response['HealthCheckObservations']
+    ])
 
 
-def _get_health_check_ids():
+def _get_health_checks():
     marker = True
-    health_check_ids = []
+    health_checks = []
     while marker:
         response = route53.list_health_checks()
         marker = response.get('NextMarker')
-        health_check_ids += [ele['Id'] for ele in response['HealthChecks']]
-    return health_check_ids
+        health_checks += response['HealthChecks']
+    health_check_ids = [ele['Id'] for ele in health_checks]
 
-
-def _ids_to_tag_mappings(health_check_ids):
-    mapping = dict()
+    name_mapping = dict()
     while health_check_ids:
         batch = health_check_ids[:10]
         health_check_ids = health_check_ids[10:]
-        response = route53.list_tags_for_resources(
-            ResourceType='healthcheck',
-            ResourceIds=batch
-        )
+        response = route53.list_tags_for_resources(ResourceType='healthcheck', ResourceIds=batch)
         for tag_set in response['ResourceTagSets']:
             resource_id = tag_set['ResourceId']
             name = _find_first(lambda s: s['Key'] == 'Name', tag_set['Tags'])
             if name:
-                mapping[name['Value']] = resource_id
-    return mapping
+                name_mapping[resource_id] = name['Value']
+
+    id_health_mapping = dict()
+    for health_check in [ele for ele in health_checks if ele['HealthCheckConfig']['Type'] != 'CALCULATED']:
+        status = _get_health_check_status(health_check['Id'])
+        health_check['Status'] = status
+        id_health_mapping[health_check['Id']] = status
+
+    calculated_health_check_ids = set([])
+    for health_check in [ele for ele in health_checks if ele['HealthCheckConfig']['Type'] == 'CALCULATED']:
+        calculated_health_check_ids.add(health_check['Id'])
+        status = all(id_health_mapping[i] for i in health_check['HealthCheckConfig']['ChildHealthChecks'])
+        id_health_mapping[health_check['Id']] = status
+
+    return [
+        HealthCheck(
+            name=name_mapping[k],
+            id=k,
+            healthy=v,
+            availability=_availability(k) if k not in calculated_health_check_ids else None
+        ) for k, v in id_health_mapping.items()
+    ]
+
